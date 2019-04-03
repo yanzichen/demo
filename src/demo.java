26b4378b831392c6f6a2ff817878e439050768dd
yanzichen <48895967+yanzichen@users.noreply.github.com>
2019-04-03 16:11:23 +0800

    Update demo.java

--- src/demo.java
+++ src/demo.java
@@ -3,5 +3,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("测试1");
 		System.out.println("测试2");
+		System.out.println("测试3");
+		System.out.println("测试4");
 	}
 }
